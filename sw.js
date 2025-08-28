23e648c9851810688625f1a60c454e3754b9426a
dmdjlorenz <dmdjlorenz@gmail.com>
2025-08-28 21:19:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
 const CACHE_NAME = 'todo-list-cache-v1';
 const urlsToCache = [
     '/',
-    'index.html'
+    '/index.html',
+    '/manifest.json',
+    '/icone.png'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
+                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
+            .catch(err => console.error('Cache addAll failed', err))
     );
 });
 
@@ -17,7 +21,24 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return response || fetch(event.request);
+                return response || fetch(event.request).catch(() => {
+                    return caches.match('/index.html');
+                });
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
